92ee9779cf6f9b4b22272ac82c2e6640baaa689e
Vedant Dhadge <107037206+dhadgevedant@users.noreply.github.com>
2025-04-09 22:52:12 +0530

    landing buttons products

--- scripts.js
+++ scripts.js
@@ -11,6 +11,32 @@ menuButton.addEventListener('click', () => {
     }
 });
 
+window.addEventListener('scroll', () => {
+    // Get the scroll percentage
+    const scrollPercentage = window.scrollY / (document.documentElement.scrollHeight - window.innerHeight);
+
+    // Calculate the offset values for prompt and tool
+    const promptOffset = 100 * scrollPercentage; // Moves out from left
+    const toolOffset = 100 * scrollPercentage; // Moves out from right
+    // console.log(scrollPercentage);
+    // Apply the transformations based on scroll percentage
+    prompt.style.transform = ` translateX(-${promptOffset}%)`;
+    tool.style.transform = ` translateX(${toolOffset}%)`;
+
+});
+
+// const landingContact = document.getElementById('landingContact');
+
+// gsap.from(landingContact, {
+  
+//   delay:2,
+//   y:100,
+//   ease: 'elastic',
+//   duration: 2,
+  
+  
+// });
+
 
 
 
@@ -48,85 +74,9 @@ gsap.to(slider,{
   }
 });
 
-window.addEventListener('scroll', () => {
-    // Get the scroll percentage
-    const scrollPercentage = window.scrollY / (document.documentElement.scrollHeight - window.innerHeight);
-
-    // Calculate the offset values for prompt and tool
-    const promptOffset = 100 * scrollPercentage; // Moves out from left
-    const toolOffset = 100 * scrollPercentage; // Moves out from right
-    // console.log(scrollPercentage);
-    // Apply the transformations based on scroll percentage
-    prompt.style.transform = ` translateX(-${promptOffset}%)`;
-    tool.style.transform = ` translateX(${toolOffset}%)`;
-
-    // if(scrollPercentage>0 && scrollPercentage<1){
-      // slider.style.transform = `translateX(-${scrollPercentage*100}%)`
-      
-
-      // });
-     
-    // }
-
-
-});
 
 
 
 
 
 
-// const container = document.querySelector(".container");
-// const sections = gsap.utils.toArray(".container section");
-// const texts = gsap.utils.toArray(".anim");
-// const mask = document.querySelector(".mask");
-
-// let scrollTween = gsap.to(sections, {
-//   xPercent: -100 * (sections.length - 1) *2,
-//   ease: "none",
-//   scrollTrigger: {
-//     trigger: ".container",
-//     pin: true,
-//     scrub: 1,
-//     end: "+=3000",
-//     //snap: 1 / (sections.length - 1),
-//     markers: true,
-//   }
-// });
-
-// console.log(1 / (sections.length - 1))
-
-// //Progress bar animation
-
-// gsap.to(mask, {
-//   width: "100%",
-//   scrollTrigger: {
-//     trigger: "#wrapper",
-//     start: "top left",
-//     scrub: 1
-//   }
-// });
-
-// // whizz around the sections
-// sections.forEach((section) => {
-//   // grab the scoped text
-//   let text = section.querySelectorAll(".anim");
-  
-//   // bump out if there's no items to animate
-//   if(text.length === 0)  return 
-  
-//   // do a little stagger
-//   gsap.from(text, {
-//     y: -130,
-//     opacity: 0,
-//     duration: 2,
-//     ease: "elastic",
-//     stagger: 0.1,
-//     scrollTrigger: {
-//       trigger: section,
-//       containerAnimation: scrollTween,
-//       start: "left center",
-//       markers: true
-//     }
-//   });
-// });
